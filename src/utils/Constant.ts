0fc8d5d2aa366fd9b2fdf0ecee645b602a86bbc8
Debiancc <wtf@debiancc.me>
2018-09-22 16:49:59 +0800

    [refactor]: Remove unnecessary switch case logic in Constant.ts

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -62,10 +62,6 @@ export const getRadius = (node: NodeData | any, type?: TYPE | any): number => {
   switch (node ? node.type : type) {
     case TYPE.TIKV_INSTANCE:
       return 10;
-    case TYPE.PD_INSTANCE:
-      return 10;
-    case TYPE.TIDB_INSTANCE:
-      return 10;
     case TYPE.FOLLOW_REGION:
     case TYPE.LEADER_REGION:
     case TYPE.UNUSED_REGION:
