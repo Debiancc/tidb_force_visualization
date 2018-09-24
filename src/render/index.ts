d83ead9fc79c2bb4794f60c647470341dfb75861
Debiancc <wtf@debiancc.me>
2018-09-24 13:36:02 +0800

    [fix]: Remove debug code

--- src/render/index.ts
+++ src/render/index.ts
@@ -4,9 +4,9 @@ import {
   getRadius,
   LinkData,
   NodeData,
-  SvgChild
-} from "../utils/Constant";
-import * as d3 from "d3";
+  SvgChild,
+} from '../utils/Constant'
+import * as d3 from 'd3'
 
 /**
  * Render class
@@ -16,159 +16,158 @@ import * as d3 from "d3";
  */
 export default class Render {
   constructor(svgSelector: string, data: Data, width: number, height: number) {
-    this.data = this.watch<Data>(data);
-    // this.data = data;
-    this.width = width;
-    this.height = height;
-    this.svgElement = d3.select(svgSelector);
-    this.simulation = this.initLayout();
-    this.svgChilds = this.initNode();
+    this.data = this.watch<Data>(data)
+    this.width = width
+    this.height = height
+    this.svgElement = d3.select(svgSelector)
+    this.simulation = this.initLayout()
+    this.svgChilds = this.initNode()
   }
 
-  public readonly simulation: d3.Simulation<NodeData, undefined>;
-  public data: Data;
+  public readonly simulation: d3.Simulation<NodeData, undefined>
+  public data: Data
 
-  private readonly svgChilds: SvgChild;
-  private readonly svgElement: d3.Selection<SVGElement, null, HTMLElement, any>;
-  private readonly width: number;
-  private readonly height: number;
+  private readonly svgChilds: SvgChild
+  private readonly svgElement: d3.Selection<SVGElement, null, HTMLElement, any>
+  private readonly width: number
+  private readonly height: number
 
   private watch = <T extends {}>(obj: T): T => {
     return new Proxy(obj, {
       get: (target, key, receiver) => {
-        const value = Reflect.get(target, key, receiver);
+        const value = Reflect.get(target, key, receiver)
         if (Array.isArray(value)) {
-          return this.watch(value);
+          return this.watch(value)
         }
-        return value;
+        return value
       },
       set: (target, key, value, receiver) => {
-        const success = Reflect.set(target, key, value, receiver);
-        success && this.reflow();
-        return success;
-      }
-    });
-  };
+        const success = Reflect.set(target, key, value, receiver)
+        success && this.reflow()
+        return success
+      },
+    })
+  }
 
   private initNode = () => {
     return {
       link: this.svgElement
-        .append<SVGGElement>("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 1)
-        .selectAll("line")
+        .append<SVGGElement>('g')
+        .attr('stroke', '#999')
+        .attr('stroke-opacity', 1)
+        .selectAll('line')
         .data(this.data.links)
         .enter()
-        .append<SVGLineElement>("line")
-        .attr("stroke-width", d => d.value),
+        .append<SVGLineElement>('line')
+        .attr('stroke-width', d => d.value),
 
       node: this.svgElement
-        .append<SVGGElement>("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-        .selectAll<SVGCircleElement, NodeData>("circle")
+        .append<SVGGElement>('g')
+        .attr('stroke', '#fff')
+        .attr('stroke-width', 1.5)
+        .selectAll<SVGCircleElement, NodeData>('circle')
         .data<NodeData>(this.data.nodes)
         .enter()
-        .append<SVGCircleElement>("circle")
-        .attr("r", getRadius)
-        .attr("fill", getColor)
-        .attr("class", (d, index) => {
-          const id = this.data.nodes[index].id.split("-");
-          id.pop();
-          return id.join("-");
+        .append<SVGCircleElement>('circle')
+        .attr('r', getRadius)
+        .attr('fill', getColor)
+        .attr('class', (d, index) => {
+          const id = this.data.nodes[index].id.split('-')
+          id.pop()
+          return id.join('-')
         })
         .call(
           d3
             .drag<SVGCircleElement, NodeData>()
-            .on("start", d => {
-              if (!d3.event.active) this.simulation.alphaTarget(0.1).restart();
-              d.fx = d.x;
-              d.fy = d.y;
+            .on('start', d => {
+              if (!d3.event.active) this.simulation.alphaTarget(0.1).restart()
+              d.fx = d.x
+              d.fy = d.y
             })
-            .on("drag", function(d) {
-              d.fx = d3.event.x;
-              d.fy = d3.event.y;
+            .on('drag', function(d) {
+              d.fx = d3.event.x
+              d.fy = d3.event.y
             })
-            .on("end", d => {
-              if (!d3.event.active) this.simulation.alphaTarget(0);
-              d.fx = null;
-              d.fy = null;
+            .on('end', d => {
+              if (!d3.event.active) this.simulation.alphaTarget(0)
+              d.fx = null
+              d.fy = null
             })
-        )
-    };
-  };
+        ),
+    }
+  }
 
   private initLayout = () => {
     return d3
       .forceSimulation<NodeData>(this.data.nodes)
       .force(
-        "link",
+        'link',
         d3
           .forceLink<NodeData, LinkData>(this.data.links)
           .id(d => d.id)
           .distance(20)
       )
-      .force("charge_force", d3.forceManyBody().strength(-100))
-      .force("center", d3.forceCenter(this.width / 2, this.height / 2))
-      .on("tick", () => {
-        if (!this.svgChilds.link || !this.svgChilds.node) return;
-        const self = this;
+      .force('charge_force', d3.forceManyBody().strength(-100))
+      .force('center', d3.forceCenter(this.width / 2, this.height / 2))
+      .on('tick', () => {
+        if (!this.svgChilds.link || !this.svgChilds.node) return
+        const self = this
         this.svgChilds.link
-          .attr("x1", d => d.source.x || 0)
-          .attr("y1", d => d.source.y || 0)
-          .attr("x2", d => d.target.x || 0)
-          .attr("y2", d => d.target.y || 0);
+          .attr('x1', d => d.source.x || 0)
+          .attr('y1', d => d.source.y || 0)
+          .attr('x2', d => d.target.x || 0)
+          .attr('y2', d => d.target.y || 0)
         this.svgChilds.node
-          .attr("cx", function(d) {
+          .attr('cx', function(d) {
             return (d.x = Math.max(
               this.r.baseVal.value,
               Math.min(self.width - this.r.baseVal.value, d.x || 0)
-            ));
+            ))
           })
-          .attr("cy", function(d) {
+          .attr('cy', function(d) {
             return (d.y = Math.max(
               this.r.baseVal.value,
               Math.min(self.height - this.r.baseVal.value, d.y || 0)
-            ));
-          });
-      });
-  };
+            ))
+          })
+      })
+  }
 
   public reflow = () => {
     this.svgChilds.node = this.svgChilds.node.data<NodeData>(
       this.data.nodes,
       function(d) {
-        return d.id;
+        return d.id
       }
-    );
-    this.svgChilds.node.exit<NodeData>().remove();
+    )
+    this.svgChilds.node.exit<NodeData>().remove()
     this.svgChilds.node = this.svgChilds.node
       .enter()
-      .append<SVGCircleElement>("circle")
-      .attr("fill", getColor)
-      .attr("r", getRadius)
-      .merge(this.svgChilds.node);
+      .append<SVGCircleElement>('circle')
+      .attr('fill', getColor)
+      .attr('r', getRadius)
+      .merge(this.svgChilds.node)
 
     this.svgChilds.link = this.svgChilds.link.data(this.data.links, function(
       d
     ) {
-      return d.source.id + "-" + d.target.id;
-    });
-    this.svgChilds.link.exit().remove();
+      return d.source.id + '-' + d.target.id
+    })
+    this.svgChilds.link.exit().remove()
     this.svgChilds.link = this.svgChilds.link
       .enter()
-      .append<SVGLineElement>("line")
-      .merge(this.svgChilds.link);
+      .append<SVGLineElement>('line')
+      .merge(this.svgChilds.link)
 
-    this.simulation.nodes(this.data.nodes);
+    this.simulation.nodes(this.data.nodes)
     this.simulation.force(
-      "link",
+      'link',
       d3
         .forceLink<NodeData, LinkData>(this.data.links)
         .id(d => d.id)
         .distance(20)
-    );
-    this.simulation.alpha(1).restart();
-    return this;
-  };
+    )
+    this.simulation.alpha(1).restart()
+    return this
+  }
 }
