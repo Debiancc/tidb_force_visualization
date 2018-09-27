5d0b0f18cb49dafb5db2002acf00bb50eaf9f279
Debiancc <wtf@debiancc.me>
2018-09-27 23:06:52 +0800

    [test]: Fix when run twice same suite test

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
-const { jestPreset: tsJestPreset } = require('ts-jest')
-
 module.exports = {
-  preset: 'ts-jest',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupTestFrameworkScriptFile: '<rootDir>/src/setupEnzyme.ts',
 }
