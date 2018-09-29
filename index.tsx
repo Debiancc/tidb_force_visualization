4d932286d8c121963d1c38b1e96e0fd3853dd4c3
Debiancc <wtf@debiancc.me>
2018-09-29 22:34:08 +0800

    [fix]: Update least dist

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src/app";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/app'
 
-const mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+const mountNode = document.getElementById('app')
+ReactDOM.render(<App />, mountNode)
